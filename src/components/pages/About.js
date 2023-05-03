149c9b20cbd87f56e9db7d43dcd5b31cf8a22a00
jsdrever <jsdrever09@gmail.com>
2023-05-03 15:04:30 -0500

    bones added to skele

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="aboutMoi">
+      <h1>About Page</h1>
+      <p>
+        I graduated from Bremen High School, where I won the superlative for wittiest, homecoming king, and a wrestling state championship.
+        I attended Darton College and managed to achieve Academic All-American both years and the Warrior Award from the athletic dept. There I attained an associates degree in psychology.
+        Life University granted me the opportunity to place fifth in the nation for the NAIA wrestling division and I graduated summa cumme laude for dual diplomas. I received a bachelors in cognitive psychology, an associates in co-active life coaching, and the Scholastic Award for student athletes.
+      </p>
+    </div>
+  );
+}
