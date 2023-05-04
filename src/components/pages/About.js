25a661363f99cb618b3acebfe37b5ff399de272e
jsdrever <jsdrever09@gmail.com>
2023-05-03 22:12:50 -0500

    fixing up the about me a bit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function About() {
   return (
 <div className="aboutMoi">
+          <h3 className="card-title">About Me</h3>
   <div className="row">
     <div className="col s4 m7">
       <div className="card">
         <div className="card-image">
           <img src={ require("../../images/about-me.jpeg") } alt='Password Generator' />
-          <span className="card-title">About Me</span>
         </div>
         <div className="card-content">
           <p>
@@ -17,9 +17,6 @@ export default function About() {
             Life University granted me the opportunity to place fifth in the nation for the NAIA wrestling division and I graduated summa cumme laude for dual diplomas. I received a bachelors in cognitive psychology, an associates in co-active life coaching, and the Scholastic Award for student athletes.
           </p>
         </div>
-        <div className="card-action">
-          <a href="www.youtube.com">This is a link</a>
-        </div>
       </div>
     </div>
 </div>
@@ -27,12 +24,44 @@ export default function About() {
   <div className="col s12 m7">
     <div className="card">
       <div className="card-image">
-        <img src="images/sample-1.jpg" />
+        <img src={ require("../../images/dualdiploma.jpeg") } alt='dual diploma'/>
       </div>
     </div>
   </div>
 </div>
+<div id="work-me" className="row">
+    <div className="work-header"> <h5> Magnus Alumni </h5></div>
+    <div className="col s4 m6">
+      <div className="card">
+        <div className="card-image">
+          <img src={ require("../../images/blue devil.webp")} alt='Bremen High School'/>
+        </div>
+      </div>
+    </div>
+    <div className="col s4 m6">
+      <div className="card">
+        <div className="card-image">
+          <img src={ require("../../images/darton state college.png")} alt='Darton College'/>
+        </div>
+      </div>
+    </div>
+    {/* tic tac toe */}
+    <div className="col s4 m6">
+      <div className="card ">
+        <div className="card-image">
+          <img src={ require("../../images/lifeu.png") } alt='Life University' />
+      </div>
+    </div>
+  </div>
 
+    <div className="col s4 m6">
+      <div className="card ">
+        <div className="card-image">
+          <img src={ require("../../images/utexas.png") } alt='University of Texas' />
+      </div>
+    </div>
+  </div>
+  </div>
 </div>
   );
   }
