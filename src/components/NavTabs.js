5466df8538bc1750c3b6a0d7178b779c14a4ce43
jsdrever <jsdrever09@gmail.com>
2023-05-07 14:30:15 -0500

    sorted pics added message section to contact me

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -13,7 +13,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
-          Home
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
@@ -33,7 +33,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Resume
         </a>
       </li>
       <li className="nav-item">
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,6 +5,29 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <h3 class="contact" id="contact-me">Contact Me</h3>
+      <section id="contact">
+  <h2 className="text-center">Contact Me</h2>
+  <form>
+    <ul>
+      <li>
+        <label className="text-right" htmlFor="name">Name:</label>
+        <input type="text" id="name" name="user-name" />
+      </li>
+      <li>
+        <label className="text-right" htmlFor="email">Email:</label>
+        <input type="text" id="email" name="user-email" />
+      </li>
+    </ul>
+    <br />
+    <label className="text-right" htmlFor="name">A short description of desired services:</label>
+    <input type="text" id="quantity" name="quantity" />
+    <br />
+    <div className="text-center">
+      <button type="submit">SEND</button>
+    </div>
+  </form>
+</section>
+
             <p>&copy; WebWizardWarrior 1990</p>
             <a href="https://youtu.be/6WTdTwcmxyo">XXX-867-5309</a>
           <br></br>
